d268f0fb85b3a79650cd1569bcf004d735b498fb
Buhai Darius <buhaidarius@gmail.com>
2018-01-25 11:02:53 +0200

    no message

--- main_app/models.py
+++ main_app/models.py
@@ -18,4 +18,15 @@ class Diets(models.Model):
     image = models.CharField(default = "",max_length=100)
 
     def __str__(self):
+        return self.name + self.id
+
+class Days(models.Model):
+    name = models.CharField(max_length=100)
+    day = models.IntegerField()
+    diet = models.IntegerField()
+    breakfast = models.CharField(max_length=100)
+    lunch = models.CharField(max_length=100)
+    dinner = models.CharField(max_length=100)
+
+    def __str__(self):
         return self.name
